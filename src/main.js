37f0c092a534854d1d2cf4aa89631a1eef0025f1
Dj0nny <frank96990@gmail.com>
2019-02-28 15:28:41 +0100

    integrated Ionic framework

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import Ionic from "@ionic/vue";
+
+Vue.use(Ionic);
+
 Vue.config.productionTip = false;
 
 new Vue({
