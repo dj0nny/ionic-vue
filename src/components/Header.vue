bcb1d6d44c2221e716a000c1cec17a4c07fef377
Dj0nny <frank96990@gmail.com>
2019-02-28 15:48:45 +0100

    delete useless components, add Header component, add logo

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="nav">
+    <ion-grid>
+      <ion-row>
+        <ion-col size="12">
+          <router-link to="/">Reddit's posts</router-link>|
+          <router-link to="/about">About this repo</router-link>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style lang="scss" scoped>
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
